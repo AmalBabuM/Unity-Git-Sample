40e799c4387738561b325ead9b45dcef489f9e34
Amal Babu <amalb0220@gmail.com>
2023-02-07 14:54:10 +0530

    Pallet file is modified with some text and script

--- Assets/pallet/items.cs
+++ Assets/pallet/items.cs
@@ -7,7 +7,7 @@ public class items : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Okay i have edited in second file"); 
     }
 
     // Update is called once per frame
